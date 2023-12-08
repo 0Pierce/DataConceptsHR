4bfe5652facc9dfd30c0635773f7b59e7ba1f3d2
Pierce <123277032+0Pierce@users.noreply.github.com>
2023-12-08 03:37:24 -0500

    Pain

--- Backend/server.js
+++ Backend/server.js
@@ -23,21 +23,32 @@ async function getDatabaseConnection() {
   try {
     const conn = await oracledb.getConnection(config);
     console.log("Connected to the School database!");
-
-    const result = await conn.execute("SELECT * FROM HR_EMPLOYEES");
+    const query = `
+    SELECT E.*, J.*, D.*
+    FROM HR_EMPLOYEES E
+    JOIN HR_JOBS J ON E.JOB_ID = J.JOB_ID
+    JOIN HR_DEPARTMENTS D ON E.DEPARTMENT_ID = D.DEPARTMENT_ID
+  `;
+    const result = await conn.execute(query);
     const HR_Employees = result.rows.map((row) => ({
       employee_ID: row[0],
       fName: row[1],
-      lName: row[2], // Fix the typo here
+      lName: row[2],
       email: row[3],
       phone_Num: row[4],
       hire_Date: row[5],
-      job_ID: row[6], // Fix the square brackets here
+      job_ID: row[6],
       salary: row[7],
       comm: row[8],
-      manager_ID: row[9], // Fix the typo here
+      manager_ID: row[9],
       dept_ID: row[10],
+      job_Title: row[11],
+      dept_Name: row[12],
+      min_Salary: row[13],
+      max_Salary: row[14],
+      location_ID: row[15],
     }));
+    console.log(HR_Employees);
     return HR_Employees;
   } catch (err) {
     console.log("Connection error:", err);
--- Frontend/dataProjFront/src/pages/Employees.jsx
+++ Frontend/dataProjFront/src/pages/Employees.jsx
@@ -5,24 +5,64 @@ import Footer from "/src/components/Footer.jsx"
 
 function Employees() {
     
-
-
+  //==============================================
+  //NOTE:
+  /* 
+  I believe the firstName and last name should probably automatically pop up
+  when you select one of them, however, that isnt listed in the assignment documents and I also been awake
+  for 2 days shuffling between projects, so I wont be able to include that, also considering only two of us
+  are working on the project.
+   */
+  //==============================================
+  const [data, setData] = useState(null); // Define data state
     const [jobIds, setJobIds] = useState([]);
     const [jobTitles, setJobTitles] = useState([])
-
+    const [employeeID, setEmployeeID] = useState([])
+    const [fName, setfName] = useState([])
+    const [lName, setlName] = useState([])
+    const [deptID, setdeptID] = useState([])
+    const [deptName, setdeptName] = useState([])
+    const [selectedEmployee, setSelectedEmployee] = useState(null);
+    const [selectedEmployeeName, setSelectedEmployeeName] = useState('');
     useEffect(() => {
         const fetchJobIds = async () => {
           try {
             const response = await fetch('/api');
             const data = await response.json();
-      
+            setData(data);
             // Use Set to store unique job IDs
             const uniqueJobIds = new Set(data.Employees.map(employee => employee.job_ID));
-      
             // Convert Set back to an array
             const uniqueJobIdsArray = [...uniqueJobIds];
-      
             setJobIds(uniqueJobIdsArray);
+
+            const uniqueJobTitles = new Set(data.Employees.map(employee => employee.job_Title));
+            const uniqueJobTitlesArray = [...uniqueJobTitles];
+            setJobTitles(uniqueJobTitlesArray);
+
+            const uniqueEmpID = new Set(data.Employees.map(employee => employee.employee_ID));
+            const uniqueEmpIDArray = [...uniqueEmpID];
+            setEmployeeID(uniqueEmpIDArray);
+
+            const uniquefName = new Set(data.Employees.map(employee => employee.fName));
+            const uniquefNameArray = [...uniquefName];
+            setfName(uniquefNameArray);
+
+            const uniquelName = new Set(data.Employees.map(employee => employee.lName));
+            const uniquelNameArray = [...uniquelName];
+            setlName(uniquelNameArray);
+
+            const uniquedeptID = new Set(data.Employees.map(employee => employee.dept_ID));
+            const uniquedeptIDArray = [...uniquedeptID];
+            setdeptID(uniquedeptIDArray);
+
+            const uniquedeptName = new Set(data.Employees.map(employee => employee.dept_Name));
+            const uniquedeptNameArray = [...uniquedeptName];
+            setdeptName(uniquedeptNameArray);
+            
+            
+            
+            
           } catch (error) {
             console.error('Error fetching data:', error);
           }
@@ -31,13 +71,66 @@ function Employees() {
         fetchJobIds();
       }, []);
 
+      function editEmployee(id){
+        console.log(id);
+
+        const clickedEmployee = data.Employees.find(employee => employee.employee_ID === id);
+    
+        setSelectedEmployee(clickedEmployee);
+        setSelectedEmployeeName(`${clickedEmployee?.fName} ${clickedEmployee?.lName}`);
+      }
 
   return (
     <>
    <Header/>
 
    <div className="employeeBody">
-        
+        <div className="editEmployeesBody">
+        <div className="employeeList">
+        <ul>
+    {employeeID.map((id, index) => (
+      <li key={id} onClick={() => editEmployee(id)}>
+        {fName[index]} {lName[index]} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  {id}
+      </li>
+    ))}
+  </ul>
+        </div>
+        <div className="editEmployeeForm">
+          <h1>Edit employee</h1>
+          <h3>Name:{selectedEmployeeName}</h3>
+          <div className="empFields">
+
+          <label htmlFor="salary">Salary:</label>
+              <div className="empField">
+             
+              <input type="text" placeholder="Salary" id='salary' value={selectedEmployee?.salary ?? 'Null'} />
+              <input type="text" name="" id="" placeholder='new Salary' />
+              </div>
+              <label htmlFor="email">Email:</label>
+              <div className="empField">
+             
+              <input type="text" placeholder="Email" id='email' value={selectedEmployee?.email ?? 'Null'} />
+              <input type="text" name="" id="" placeholder='new Email' />
+              </div>
+              <label htmlFor="phone">Phone:</label>
+              <div className="empField">
+              <input type="text" placeholder="Phone" id='phone' value={selectedEmployee?.phone_Num ?? 'Null'} />
+              <input type="text" name="" id="" placeholder='new Phone' />
+              </div>
+          </div>
+
+          
+
+        </div>
+        </div>
+
+
+
+
+
+
+
+
         <div className="employeeForm">
         <form>
         <h2>Employee Hiring Form</h2>
@@ -66,38 +159,39 @@ function Employees() {
             <label htmlFor="JOB_TITLE">JOB TITLE</label>
              <select name="JOB TITLE" id="JOB_TITLE" required>
              <option value="" disabled selected>Select Manager ID</option>
-             <option value="Q1">Q1</option>
-             <option value="Q2">Q2</option>
+             {jobTitles.map(jobTitles => <option key={jobTitles} value={jobTitles}>{jobTitles}</option>)}
             
             </select>
 
            </div>
         </div>
+        
         <div className="manInfo">
-            
+          <h3>Manager: </h3>
+        <div className="manInfoContent">
+
+
             <div className="empID">
                 <label htmlFor="EMPLOYEE_ID">Employee ID</label>
             <select name="EMPLOYEE_ID" id="EMPLOYEE_ID" required>
             <option value="" disabled selected>Select Manager ID</option>
-             <option value="Q1">Q1</option>
-             <option value="Q2">Q2</option>
+            {employeeID.map(employeeID => <option key={employeeID} value={employeeID}>{employeeID}</option>)}
             </select>
             </div>
             <div className="fNameInfo">
             <label htmlFor="FIRST_NAME">First Name</label>
             <select name="FIRST_NAME" id="FIRST_NAME" required>
             <option value="" disabled selected>Select First Name</option>
-             <option value="Q1">Q1</option>
-             <option value="Q2">Q2</option>
+            {fName.map(fName => <option key={fName} value={fName}>{fName}</option>)}
             </select>
             </div>
             <div className="lNameInfo">
             <label htmlFor="LAST_NAME">Last Name</label>
             <select name="LAST_NAME" id="LAST_NAME" required>
             <option value="" disabled selected>Select Last Name</option>
-             <option value="Q1">Q1</option>
-             <option value="Q2">Q2</option>
+            {lName.map(lName => <option key={lName} value={lName}>{lName}</option>)}
             </select>
+            </div>
             </div>
         </div>
 
@@ -106,20 +200,19 @@ function Employees() {
                 <label htmlFor="DEPARTMENT_ID">Department ID</label>
                 <select name="DEPARTMENT_ID" id="DEPARTMENT_ID" required>
                 <option value="" disabled selected>Select Manager ID</option>
-                 <option value="Q1">Q1</option>
-                 <option value="Q2">Q2</option>
+                {deptID.map(deptID => <option key={deptID} value={deptID}>{deptID}</option>)}
                 </select>
             </div>
             <div className="deptName">
             <label htmlFor="DEPARTMENT_NAME">Department Name</label>
             <select name="DEPARTMENT_NAME" id="DEPARTMENT_NAME" required>
             <option value="" disabled selected>Select Dept.. Name</option>
-             <option value="Q1">Q1</option>
-             <option value="Q2">Q2</option>
+            {deptName.map(deptName => <option key={deptName} value={deptName}>{deptName}</option>)}
+
             </select>
             </div>
         </div>
-        <div className="smbBtn"><input type="submit"></input></div>
+        <div className="smbBtn"><input type="submit" value="Hire"></input></div>
         
     </form>
     </div>
@@ -129,7 +222,7 @@ function Employees() {
    
    
 
-    <Footer/>
+   <Footer/>
     </>
     
   )
