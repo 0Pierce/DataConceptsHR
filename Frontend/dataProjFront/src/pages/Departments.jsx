152bb92115b587e5ea19ab1b33671422aa1b7e7c
MatviiKolomiiets <147009029+MatviiKolomiiets@users.noreply.github.com>
2023-12-08 20:51:56 -0500

    Departments.jsx

--- Frontend/dataProjFront/src/pages/Departments.jsx
+++ Frontend/dataProjFront/src/pages/Departments.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import React, { useState, useEffect } from 'react';
 
 const DepartmentsMenu = () => {
   const [departments, setDepartments] = useState([]);
@@ -107,6 +106,7 @@ const DepartmentsMenu = () => {
 };
 
 export default DepartmentsMenu;
+
 
 
 
